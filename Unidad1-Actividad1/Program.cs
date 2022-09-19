af082cfc956052ebd5deea5f4477e4e9c1884e54
Rodrigo <alejandroguzmancruz@outlook.com>
2022-09-18 22:09:05 -0500

    cambios

--- Unidad1-Actividad1/Program.cs
+++ Unidad1-Actividad1/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(); //activar mvc como patrón de desarrollo
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); //necesario para regresar estilos css
+app.UseRouting();
+app.UseFileServer();
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute()); // map default 
 
 app.Run();
